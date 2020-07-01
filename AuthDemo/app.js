4488732cc73cc5192a046dec9e9d90d8d0437f69
Palaash Atri <palaash.atri@gmail.com>
2020-07-01 20:46:08 +0530

    add user model, initialize passport authentication

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,10 +1,31 @@
-var express = require("express");
-     mongoose = require("mongoose");
+var express = require("express"),
+     mongoose = require("mongoose"),
+     passport = require("passport"),
+     bodyParser = require("body-parser"),
+     User = require("./models/user"),
+     LocalStrategy = require("passport-local"),
+     passportLocalMongoose = require("passport-local-mongoose");
+
+
 mongoose.connect("mongodb://localhost/auth_demo_app",{useNewUrlParser:true, useUnifiedTopology:true});
 
 var app = express();
 app.set('view engine','ejs');
 
+app.use(require("express-session")({
+    secret : "Rusty is the best and the cutest dog in the world!", 
+    resave : false,
+    saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//======
+//ROUTES
+//====== 
 app.get('/', function(req, res){
     res.render('home');
 });
@@ -13,6 +34,13 @@ app.get('/secret',function(req,res){
     res.render("secret");
 });
 
+
+//AUTH ROUTES
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+//======
 app.listen(process.env.PORT || 5000, function(){
     console.log("server has started");
 });
