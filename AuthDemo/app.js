f4c82c6b5865036542a900e5d59798565407f84f
Palaash Atri <palaash.atri@gmail.com>
2020-07-01 20:18:12 +0530

    authdemo init

---
+++ AuthDemo/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+     mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/auth_demo_app",{useNewUrlParser:true, useUnifiedTopology:true});
+
+var app = express();
+app.set('view engine','ejs');
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/secret',function(req,res){
+    res.render("secret");
+});
+
+app.listen(process.env.PORT | 5000, function(){
+    console.log("server has started");
+});
